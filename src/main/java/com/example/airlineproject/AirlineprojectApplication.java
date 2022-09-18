79c08a6a768c856cb876d92417c1d9e355a14bb2
Ba Choi <bada@Bas-MacBook-Pro.local>
2022-09-18 19:04:38 -0400

    Spring Boot Set Up

---
+++ src/main/java/com/example/airlineproject/AirlineprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/airlineproject/AirlineprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
